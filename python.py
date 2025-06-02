b4407f10498f9148c41635a6c7e98c9b51d4a28d
sosaaa123 <lautasosita097@gmail.com>
2025-06-01 22:53:13 -0300

    oa

--- python.py
+++ python.py
@@ -1,7 +1,25 @@
 import sqlite3
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware #me permite habilitar origenes que hagan consultas al backened
+
+#tengo que crear una cadena de origenes permitidos
 
 app = FastAPI()
+
+
+origenes = ["http://127.0.0.1:5500"] #por live server
+ #el origen de la pagina que va a realizar la consulta
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origenes, #los origenes que tengo permitidos en la lista
+    allow_methods = ["*"], # permito consultar por todos los metodos (get, post, put, delete)
+    allow_credentials = False, #no necesito credenciales, lo pongo para no olvidarme
+    allow_headers = ["*"], #permite todos los headers ¿que es un header?
+)
+
+
+
 
 conexion = sqlite3.connect("supermerk2.db")
 cursor = conexion.cursor()
@@ -13,4 +31,29 @@ cursor.execute("INSERT INTO productos (nombre, precio, stock) VALUES(?,?,?)", ("
 cursor.execute("INSERT INTO productos (nombre, precio, stock) VALUES(?,?,?)", ("Lampara", 7000, 100))
 cursor.execute("INSERT INTO productos (nombre, precio, stock) VALUES(?,?,?)", ("Cremona", 1000000, 100))"""
 
+res = cursor.execute("SELECT * FROM PRODUCTOS ")
+respuesta = res.fetchall()
 conexion.commit()
+
+
+def productos():
+    res = cursor.execute("SELECT nombre, precio FROM productos")
+    respuesta = res.fetchall()
+
+    dicProductos = []
+    for i in respuesta:
+        dicProductos.append({"nombre": i[0], "precio": i[1]})#lo convierto en diccionario con claves-valor como si fuera un json
+
+
+    return dicProductos
+
+
+
+
+
+@app.get("/")
+async def main():
+    answer = productos()
+    return answer
+
+print(productos())
